cf1cebdf69d22bcfb016e181d8ef59f15b72b056
alsky799 <77466395+alsky799@users.noreply.github.com>
2021-02-22 01:36:14 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$ua = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36";
+$session = "session=29b13fbb-f48b-4a76-8e2d-12618f0456b4.XpERuWsy-2mhRXC-G058zi5RrMo";
+$__cfduid = "__cfduid=dea69669b27ec1b0000b7cbdcf5d01a111613931898";
+$data = "&coins=doge&";
